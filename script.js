b79f6467ca59da28f2f7fd651a0c09731ad6701f
Joao Chrisostomo <maturandomassas@gmail.com>
2022-03-09 18:51:48 -0300

    adicionando dark-theme mudança de cor no body e em alguns elementos ainda falta finalizar mas esta tudo ok

--- script.js
+++ script.js
@@ -35,10 +35,9 @@ function fahrenheitKelvin() {
   elementoValorConvertidoFahrenheitParaKelvin.innerHTML = valorConvertidoFahrenheitParaKelvin;
 }
 
-const theme = document.getElementById("btn");
-console.log(theme);
+const btnTheme = document.getElementById("btn");
 const body = document.querySelector("body");
 
-// btnTheme.addEventListener("click", () => {
-//   body.classList.toggle(".btn-theme");
-// });
+btnTheme.addEventListener("click", () => {
+  body.classList.toggle("dark-theme");
+});
