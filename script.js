4d381c6041381d4cb9e63d07c4e8994a8a35baa7
Joao Chrisostomo <maturandomassas@gmail.com>
2022-03-09 18:20:22 -0300

    funcionalidades funcionando corretamente e acrescentando dark-mode

---
+++ script.js
@@ -0,0 +1,44 @@
+function celsiusFahrenheit() {
+  const celsiusFahrenheit = parseFloat(document.getElementById("valor"));
+  const converterCelsiusParaFahrenheit = celsiusFahrenheit * 1.8 + 32;
+  const elementoValorConvertidoCelsiusParaFahrenheit = document.getElementById(
+    "valorConvertidoCelsiusParaFahrenheit"
+  );
+  const valorConvertidoCelsiusParaFahrenheit =
+    "A temperatura em Fahrenheit é " + converterCelsiusParaFahrenheit;
+  elementoValorConvertidoCelsiusParaFahrenheit.innerHTML = valorConvertidoCelsiusParaFahrenheit;
+}
+
+function celsiusKelvin() {
+  const celsiusKelvin = parseFloat(document.getElementById("valor").value);
+  const converterCelsiusParaKelvin = celsiusKelvin + 273.15;
+  const elementoValorConvertidoCelsiusParaKelvin = document.getElementById(
+    "valorConvertidoCelsiusParaKelvin"
+  );
+  const valorConvertidoCelsiusParaKelvin =
+    "A temperatura em Kelvin é " + converterCelsiusParaKelvin;
+  elementoValorConvertidoCelsiusParaKelvin.innerHTML = valorConvertidoCelsiusParaKelvin;
+}
+
+function fahrenheitKelvin() {
+  const fahrenheitKelvin = parseFloat(document.getElementById("valor").value);
+  const converterFahrenheitParaCelsius = (fahrenheitKelvin - 32) / 1.8;
+  const converterFahrenheitParaKelvin = converterFahrenheitParaCelsius + 273.15;
+  const converterFahrenheitParaKelvinFixado = converterFahrenheitParaKelvin.toFixed(
+    1
+  );
+  const elementoValorConvertidoFahrenheitParaKelvin = document.getElementById(
+    "valorConvertidoFahrenheitParaKelvin"
+  );
+  const valorConvertidoFahrenheitParaKelvin =
+    "A temperatura em Kelvin é " + converterFahrenheitParaKelvinFixado;
+  elementoValorConvertidoFahrenheitParaKelvin.innerHTML = valorConvertidoFahrenheitParaKelvin;
+}
+
+const theme = document.getElementById("btn");
+console.log(theme);
+const body = document.querySelector("body");
+
+// btnTheme.addEventListener("click", () => {
+//   body.classList.toggle(".btn-theme");
+// });
